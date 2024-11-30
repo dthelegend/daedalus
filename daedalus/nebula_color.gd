105cfb7b6894eb3827b8e62c56ee19e3754c3364
Majonez57 <avecinat@gmail.com>
2024-11-30 22:38:40 +0000

    Shaders by faction

--- daedalus/nebula_color.gd
+++ daedalus/nebula_color.gd
@@ -1,18 +1,20 @@
 extends Polygon2D
 
 func apply_shader_with_color(faction: String):
-	var shader_path = str("res://shaders/", faction, ".gdshader")   
+	   
  # Remove the current shader (if any)
 	if material and material is ShaderMaterial:
 		material = null  # Detaches the current material
 
-	# Load the new shader
-	var shader = load(shader_path)
-	if shader:
-		var new_shader_material = ShaderMaterial.new()
-		new_shader_material.shader = shader
+	if faction != "None":
+		var shader_path = str("res://shaders/", faction, ".gdshader")
+		# Load the new shader
+		var shader = load(shader_path)
+		if shader:
+			var new_shader_material = ShaderMaterial.new()
+			new_shader_material.shader = shader
 
-		# Apply the new shader material to the Polygon2D
-		material = new_shader_material
-	else:
-		print("Failed to load shader from path: ", shader_path)
+			# Apply the new shader material to the Polygon2D
+			material = new_shader_material
+		else:
+			print("Failed to load shader from path: ", shader_path)
