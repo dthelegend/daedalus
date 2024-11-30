1c1dcde20dae16113211ed57d9d2e650006b4d15
Majonez57 <avecinat@gmail.com>
2024-11-30 18:05:21 +0000

    BattleUI

--- daedalus/battle_ui.gd
+++ daedalus/battle_ui.gd
@@ -2,16 +2,19 @@ extends Control
 class_name BattleUI
 
 var planet = ""
+@onready var title: Label = $Base/Title
 
-func _new(planet_name: String) -> void:
+func update_planet(planet_name:String) -> void:
 	planet = planet_name
+	title.text =  str("War on ", planet)
 	
-
+func _new(planet_name: String) -> void:
+	
+	pass
+	
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	var text = str("War on ", planet)
 	
-	get_node("Base/Title").text = text
 	
 	pass # Replace with function body.
 
